b0bcb39241fc988f695b0a4bcd331c923a493f19
Punam Chand <pcpatidar.4488@gmail.com>
2017-11-22 12:06:52 +0530

    Transaction merge with master

---
+++ app/src/main/java/com/berylsystems/buzz/activities/company/transection/bankcasedeposit/CreateBankCaseDepositActivity.java
@@ -0,0 +1,20 @@
+package com.berylsystems.buzz.activities.company.transection.bankcasedeposit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.berylsystems.buzz.R;
+
+public class CreateBankCaseDepositActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_bank_case_deposit);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/berylsystems/buzz/activities/dashboard/TransactionDashboardActivity.java
@@ -0,0 +1,61 @@
+package com.berylsystems.buzz.activities.dashboard;
+
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.berylsystems.buzz.R;
+import com.berylsystems.buzz.activities.app.BaseActivityCompany;
+import com.berylsystems.buzz.adapters.AdministrationDashboardAdapter;
+import com.berylsystems.buzz.adapters.TransactionDashboardAdapter;
+import com.berylsystems.buzz.entities.AppUser;
+import com.berylsystems.buzz.utils.LocalRepositories;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class TransactionDashboardActivity extends BaseActivityCompany {
+    @Bind(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+    @Bind(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    AppUser appUser;
+    RecyclerView.LayoutManager layoutManager;
+    TransactionDashboardAdapter mAdapter;
+
+    int[] myImageList = new int[]{R.drawable.icon_administration, R.drawable.icon_transaction, R.drawable.icon_display, R.drawable.icon_printer, R.drawable.icon_administration, R.drawable.icon_transaction, R.drawable.icon_display, R.drawable.icon_printer,R.drawable.icon_administration, R.drawable.icon_transaction, R.drawable.icon_display,};
+    private String[] title={
+            "Sales",
+            "Purchase",
+            "Sale Return",
+            "Purchase Return",
+            "Payment",
+            "Receipt",
+            "Bank Case Deposit",
+            "Bank Case Withdraw ",
+            "Income",
+            "Expence",
+            "Journal Voucher",
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generic_grid);
+        ButterKnife.bind(this);
+        appUser = LocalRepositories.getAppUser(this);
+        setAddCompany(2);
+        setAppBarTitleCompany(1,"TRANSACTION");
+        mRecyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mAdapter = new TransactionDashboardAdapter(this, title, myImageList);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(),CompanyDashboardActivity.class));
+    }
+}
