ac56c734be20d1029cca9c87d5bb065cd84ebde5
Punam Chand <pcpatidar.4488@gmail.com>
2018-01-09 10:50:09 +0530

    Delete API integrate Sales and expenses in dash board and delete API integrate sale report and purchase report in reports

--- app/src/main/java/com/berylsystems/buzz/activities/company/transaction/TransactionExpensesActivity.java
+++ app/src/main/java/com/berylsystems/buzz/activities/company/transaction/TransactionExpensesActivity.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -29,8 +30,10 @@ import com.berylsystems.buzz.adapters.ExpenceListAdapter;
 import com.berylsystems.buzz.adapters.TransactionExpenceAdapter;
 import com.berylsystems.buzz.entities.AppUser;
 import com.berylsystems.buzz.networks.ApiCallsService;
+import com.berylsystems.buzz.networks.api_response.expence.DeleteExpenceResponse;
 import com.berylsystems.buzz.networks.api_response.expence.GetExpenceResponse;
 import com.berylsystems.buzz.utils.Cv;
+import com.berylsystems.buzz.utils.EventDeleteExpence;
 import com.berylsystems.buzz.utils.LocalRepositories;
 import com.berylsystems.buzz.utils.TypefaceCache;
 
@@ -240,5 +243,56 @@ public class TransactionExpensesActivity extends RegisterAbstractActivity {
             Snackbar.make(coordinatorLayout,response.getMessage(), Snackbar.LENGTH_LONG).show();
         }
     }
+
+    @Subscribe
+    public void deleteincome(EventDeleteExpence pos){
+        appUser.delete_expence_id= pos.getPosition();
+        LocalRepositories.saveAppUser(this,appUser);
+        new AlertDialog.Builder(TransactionExpensesActivity.this)
+                .setTitle("Delete Expence")
+                .setMessage("Are you sure you want to delete this Record ?")
+                .setPositiveButton(R.string.btn_ok, (dialogInterface, i) -> {
+                    Boolean isConnected = ConnectivityReceiver.isConnected();
+                    if(isConnected) {
+                        mProgressDialog = new ProgressDialog(TransactionExpensesActivity.this);
+                        mProgressDialog.setMessage("Info...");
+                        mProgressDialog.setIndeterminate(false);
+                        mProgressDialog.setCancelable(true);
+                        mProgressDialog.show();
+                        LocalRepositories.saveAppUser(getApplicationContext(), appUser);
+                        ApiCallsService.action(getApplicationContext(), Cv.ACTION_DELETE_EXPENCE);
+                    }
+                    else{
+                        snackbar = Snackbar
+                                .make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
+                                .setAction("RETRY", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Boolean isConnected = ConnectivityReceiver.isConnected();
+                                        if(isConnected){
+                                            snackbar.dismiss();
+                                        }
+                                    }
+                                });
+                        snackbar.show();
+                    }
+                })
+                .setNegativeButton(R.string.btn_cancel, null)
+                .show();
+    }
+    @Subscribe
+    public void deleteexpenceresponse(DeleteExpenceResponse response){
+        mProgressDialog.dismiss();
+        if(response.getStatus()==200){
+            ApiCallsService.action(getApplicationContext(), Cv.ACTION_GET_EXPENCE);
+            Snackbar
+                    .make(coordinatorLayout, response.getMessage(), Snackbar.LENGTH_LONG).show();
+        }
+        else{
+            Snackbar
+                    .make(coordinatorLayout, response.getMessage(), Snackbar.LENGTH_LONG).show();
+        }
+    }
+
 
 }
--- app/src/main/java/com/berylsystems/buzz/activities/company/transaction/TransactionSalesActivity.java
+++ app/src/main/java/com/berylsystems/buzz/activities/company/transaction/TransactionSalesActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
@@ -27,8 +28,10 @@ import com.berylsystems.buzz.activities.company.FirstPageActivity;
 import com.berylsystems.buzz.adapters.TransactionSalesAdapter;
 import com.berylsystems.buzz.entities.AppUser;
 import com.berylsystems.buzz.networks.ApiCallsService;
+import com.berylsystems.buzz.networks.api_response.salevoucher.DeleteSaleVoucherResponse;
 import com.berylsystems.buzz.networks.api_response.salevoucher.GetSaleVoucherListResponse;
 import com.berylsystems.buzz.utils.Cv;
+import com.berylsystems.buzz.utils.EventDeleteSaleVoucher;
 import com.berylsystems.buzz.utils.LocalRepositories;
 import com.berylsystems.buzz.utils.TypefaceCache;
 
@@ -236,6 +239,57 @@ public class TransactionSalesActivity extends RegisterAbstractActivity {
         }
         else{
             Snackbar.make(coordinatorLayout,response.getMessage(), Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    @Subscribe
+    public void deletesalevoucher(EventDeleteSaleVoucher pos){
+        appUser.delete_sale_voucher_id= pos.getPosition();
+        LocalRepositories.saveAppUser(this,appUser);
+        new AlertDialog.Builder(TransactionSalesActivity.this)
+                .setTitle("Delete Sale Voucher")
+                .setMessage("Are you sure you want to delete this Record ?")
+                .setPositiveButton(R.string.btn_ok, (dialogInterface, i) -> {
+                    Boolean isConnected = ConnectivityReceiver.isConnected();
+                    if(isConnected) {
+                        mProgressDialog = new ProgressDialog(TransactionSalesActivity.this);
+                        mProgressDialog.setMessage("Info...");
+                        mProgressDialog.setIndeterminate(false);
+                        mProgressDialog.setCancelable(true);
+                        mProgressDialog.show();
+                        LocalRepositories.saveAppUser(getApplicationContext(), appUser);
+                        ApiCallsService.action(getApplicationContext(), Cv.ACTION_DELETE_SALE_VOUCHER);
+                    }
+                    else{
+                        snackbar = Snackbar
+                                .make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
+                                .setAction("RETRY", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Boolean isConnected = ConnectivityReceiver.isConnected();
+                                        if(isConnected){
+                                            snackbar.dismiss();
+                                        }
+                                    }
+                                });
+                        snackbar.show();
+                    }
+                })
+                .setNegativeButton(R.string.btn_cancel, null)
+                .show();
+    }
+
+    @Subscribe
+    public void deletesalevoucherresponse(DeleteSaleVoucherResponse response){
+        mProgressDialog.dismiss();
+        if(response.getStatus()==200){
+            ApiCallsService.action(getApplicationContext(), Cv.ACTION_GET_SALE_VOUCHER);
+            Snackbar
+                    .make(coordinatorLayout, response.getMessage(), Snackbar.LENGTH_LONG).show();
+        }
+        else{
+            Snackbar
+                    .make(coordinatorLayout, response.getMessage(), Snackbar.LENGTH_LONG).show();
         }
     }
 }
