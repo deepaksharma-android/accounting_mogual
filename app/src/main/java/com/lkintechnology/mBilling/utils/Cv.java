17e31934522df5bd42382b4f1149f3cbe774d864
Punamchand <punam.chand@berylsystem.com>
2018-01-12 11:17:24 +0530

    FloatingButton gone in cash-in-hand,bank account,customer,supplier and stock-in-hand

--- app/src/main/java/com/lkintechnology/mBilling/utils/Cv.java
+++ app/src/main/java/com/lkintechnology/mBilling/utils/Cv.java
@@ -4,8 +4,8 @@ package com.lkintechnology.mBilling.utils;
 public interface Cv {
 
 
-    String BASE_URL = "https://mbilling.in/api/v1/";
-  //  String BASE_URL = "http://accounts.geeksonrails.com/api/v1/";
+    //String BASE_URL = "https://mbilling.in/api/v1/";
+    String BASE_URL = "http://accounts.geeksonrails.com/api/v1/";
      //String BASE_URL = "http://192.168.1.11:3000/api/v1/";
 
 
