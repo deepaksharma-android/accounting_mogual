a4b979bb82d7a5029891600ed26d3ce48271bd64
suraj <suraj.dubey@berylsystems.com>
2018-02-07 12:15:15 +0530

    Added serial number reference page wirh UI

---
+++ app/src/main/java/com/lkintechnology/mBilling/networks/api_response/serialnumber/Attributes.java
@@ -0,0 +1,69 @@
+package com.lkintechnology.mBilling.networks.api_response.serialnumber;
+
+public class Attributes {
+    public String date;
+    public String name;
+    public String voucher_number;
+    public String sale_type;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVoucher_number() {
+        return voucher_number;
+    }
+
+    public void setVoucher_number(String voucher_number) {
+        this.voucher_number = voucher_number;
+    }
+
+    public String getSale_type() {
+        return sale_type;
+    }
+
+    public void setSale_type(String sale_type) {
+        this.sale_type = sale_type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double amount;
+    public String type;
+    public int id;
+
+}
