d2eef50597fe5c9c59188c6a5772b0ead1e1ec1b
Manjoor Alam <manjooralam28@gmail.com>
2018-03-05 16:44:52 +0530

    change in payment gst_nature_spinner

---
+++ app/src/main/java/com/lkintechnology/mBilling/activities/company/transaction/payment/CreatePaymentListActivity.java
@@ -0,0 +1,331 @@
+package com.lkintechnology.mBilling.activities.company.transaction.payment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.lkintechnology.mBilling.R;
+import com.lkintechnology.mBilling.activities.company.transaction.creditnotewoitem.AddCreditNoteItemActivity;
+import com.lkintechnology.mBilling.activities.company.transaction.debitnotewoitem.AddDebitNoteItemActivityy;
+import com.lkintechnology.mBilling.entities.AppUser;
+import com.lkintechnology.mBilling.utils.LocalRepositories;
+import com.lkintechnology.mBilling.utils.TypefaceCache;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreatePaymentListActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText etIVNNo;
+    private TextView tvSubmit,tvDate,etGST,etIGST,etCGST,etSGST,tvSGST,tvCGST,tvIGST,etDifferenceAmount,tvITC,tv_gst;
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private RelativeLayout rootLayout;
+    private int year, month, day;
+
+    Map mMap;
+    AppUser appUser;
+    String amount,spGoodsKey1,state;
+    private Spinner spChooseGoods;
+    private LinearLayout ll_submit,rootSP;
+    private double percentage,halfIC;
+    private String chooseGoods[]={" Input Goods","Input Services","Capital Goods","None"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_debit_note_item);
+        initActionbarSetup();
+        initView();
+        initialpageSetup();
+        mMap=new HashMap();
+        tvDate.setOnClickListener(this);
+        ll_submit.setOnClickListener(this);
+        amount=getIntent().getStringExtra("amount");
+        spGoodsKey1=getIntent().getStringExtra("sp_position");
+       // spGoodsKey2=getIntent().getStringExtra("sp_position");
+        state=getIntent().getExtras().getString("state");
+       // amount2=getIntent().getExtras().getString("amount");
+        if(state==null){
+            state="Haryana";
+        }
+        etDifferenceAmount.setText(amount);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+        etDifferenceAmount.setText(amount);
+        /*if (spGoodsKey1!=null) {
+            if (spGoodsKey1.equals("1")) {
+               // spChooseGoods.setVisibility(View.VISIBLE);
+                etDifferenceAmount.setText(amount2);
+                rootSP.setVisibility(View.VISIBLE);
+                tvITC.setVisibility(View.VISIBLE);
+            } else {
+                tvITC.setVisibility(View.GONE);
+            }
+        }*/
+        if (spGoodsKey1!=null) {
+            if (spGoodsKey1.equals("2")) {
+                spChooseGoods.setVisibility(View.VISIBLE);
+                rootSP.setVisibility(View.VISIBLE);
+                tvITC.setVisibility(View.VISIBLE);
+            }
+        }else {
+            tvITC.setVisibility(View.GONE);
+        }
+        if(state.equals(appUser.company_state)){
+            tvCGST.setVisibility(View.VISIBLE);
+            etCGST.setVisibility(View.VISIBLE);
+            tvSGST.setVisibility(View.VISIBLE);
+            etCGST.setVisibility(View.VISIBLE);
+            etSGST.setVisibility(View.VISIBLE);
+            tvIGST.setVisibility(View.GONE);
+            etIGST.setVisibility(View.GONE);
+            tv_gst.setText("GST %");
+
+        }
+        else{
+            tvCGST.setVisibility(View.GONE);
+            etCGST.setVisibility(View.GONE);
+            tvSGST.setVisibility(View.GONE);
+            etCGST.setVisibility(View.GONE);
+            etSGST.setVisibility(View.GONE);
+            tvIGST.setVisibility(View.VISIBLE);
+            etIGST.setVisibility(View.VISIBLE);
+            tv_gst.setText("GST %");
+        }
+
+        etGST.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length()>0){
+                    percentage= (Double.parseDouble(etDifferenceAmount.getText().toString())*Double.parseDouble(etGST.getText().toString())/100);
+                    etIGST.setText(String.valueOf(percentage));
+                    halfIC= (float) (percentage/2.0);
+                    etCGST.setText(String.valueOf(halfIC));
+                    etSGST.setText(String.valueOf(halfIC));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+    private void initialpageSetup() {
+        appUser = LocalRepositories.getAppUser(this);
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,chooseGoods);
+        spChooseGoods.setAdapter(arrayAdapter);
+        spChooseGoods.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+    // to define id here
+    private void initView() {
+        rootLayout= (RelativeLayout) findViewById(R.id.rl_add_credit_note_item);
+        etIVNNo= (EditText) findViewById(R.id.et_invoice);
+        etCGST= (TextView) findViewById(R.id.et_cgst);
+        etGST= (EditText) findViewById(R.id.et_gst);
+        etDifferenceAmount= (TextView) findViewById(R.id.et_difference_amount);
+        tvSGST= (TextView) findViewById(R.id.tv_sgst);
+        tvCGST= (TextView) findViewById(R.id.tv_cgst);
+        tvIGST= (TextView) findViewById(R.id.tv_igst);
+        etSGST= (TextView) findViewById(R.id.et_sgst);
+        tvITC= (TextView) findViewById(R.id.tv_itc);
+        tv_gst= (TextView) findViewById(R.id.tv_gst);
+        etIGST= (TextView) findViewById(R.id.et_igst);
+        spChooseGoods= (Spinner) findViewById(R.id.sp_choose_goods);
+        tvDate= (TextView) findViewById(R.id.tv_date_select);
+        ll_submit= (LinearLayout) findViewById(R.id.tv_submit);
+        rootSP= (LinearLayout) findViewById(R.id.root_sp);
+    }
+
+    //  add action bar title here
+    private void initActionbarSetup() {
+        ActionBar actionBar = getSupportActionBar();
+        View viewActionBar = getLayoutInflater().inflate(R.layout.action_bar_tittle_text_layout, null);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(//Center the textview in the ActionBar !
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                Gravity.CENTER);
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#067bc9")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(viewActionBar, params);
+        TextView actionbarTitle = (TextView) viewActionBar.findViewById(R.id.actionbar_textview);
+        actionbarTitle.setText("Create Debit Note Item");
+        actionbarTitle.setTextSize(16);
+        actionbarTitle.setTypeface(TypefaceCache.get(getAssets(),3));
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.tv_submit:
+                if (spGoodsKey1!=null) {
+                    if (spGoodsKey1.equals("1")) {
+                        spChooseGoods.setVisibility(View.INVISIBLE);
+                        if (!etIVNNo.getText().toString().equals("")) {
+                            if (!etGST.getText().toString().equals("")) {
+                                if (!tvDate.getText().toString().equals("")) {
+                                    mMap.put("inv_num", etIVNNo.getText().toString());
+                                    mMap.put("difference_amount", etDifferenceAmount.getText().toString());
+                                    mMap.put("gst", etGST.getText().toString());
+                                    if (state.equals(appUser.company_state)) {
+                                        mMap.put("cgst", etCGST.getText().toString());
+                                        mMap.put("sgst", etSGST.getText().toString());
+                                    } else {
+                                        mMap.put("igst", etIGST.getText().toString());
+                                    }
+                                    mMap.put("date", tvDate.getText().toString());
+                                    mMap.put("goodsItem", "");
+                                    appUser.mListMapForItemPaymentList.add(mMap);
+                                    LocalRepositories.saveAppUser(this, appUser);
+                                    Intent intent = new Intent(CreatePaymentListActivity.this, ShowPaymentListActivity.class);
+                                    intent.putExtra("amount", amount);
+                                    intent.putExtra("sp_position", spGoodsKey1);
+                                    intent.putExtra("state", state);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Snackbar.make(rootLayout, "please select date", Snackbar.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Snackbar.make(rootLayout, "please enter GST", Snackbar.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Snackbar.make(rootLayout, "please enter invoice number", Snackbar.LENGTH_LONG).show();
+                        }
+
+
+                    } else if (spGoodsKey1.equals("2")) {
+                        spChooseGoods.setVisibility(View.VISIBLE);
+                        if (!etIVNNo.getText().toString().equals("")) {
+                            if (!etGST.getText().toString().equals("")) {
+                                if (!tvDate.getText().toString().equals("")) {
+                                    if (!spChooseGoods.getSelectedItem().toString().equals("")) {
+                                        mMap.put("inv_num", etIVNNo.getText().toString());
+                                        mMap.put("difference_amount", etDifferenceAmount.getText().toString());
+                                        mMap.put("gst", etGST.getText().toString());
+                                        if (state.equals(appUser.company_state)) {
+                                            mMap.put("cgst", etCGST.getText().toString());
+                                            mMap.put("sgst", etSGST.getText().toString());
+                                        } else {
+                                            mMap.put("igst", etIGST.getText().toString());
+                                        }
+                                        mMap.put("date", tvDate.getText().toString());
+                                        mMap.put("goodsItem", spChooseGoods.getSelectedItem().toString());
+                                        appUser.mListMapForItemPaymentList.add(mMap);
+                                        LocalRepositories.saveAppUser(this, appUser);
+                                        Intent intent = new Intent(CreatePaymentListActivity.this, ShowPaymentListActivity.class);
+                                        intent.putExtra("amount", amount);
+                                        intent.putExtra("sp_position", spGoodsKey1);
+                                        intent.putExtra("state", state);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Snackbar.make(rootLayout, "please choose goods item", Snackbar.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Snackbar.make(rootLayout, "please select date", Snackbar.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Snackbar.make(rootLayout, "please enter GST", Snackbar.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Snackbar.make(rootLayout, "please enter invoice number", Snackbar.LENGTH_LONG).show();
+                        }
+
+
+                    }
+                }
+
+
+
+
+
+
+                break;
+            case R.id.tv_date_select:
+
+                showDialog(999);
+
+                break;
+        }
+
+    }
+
+
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        tvDate.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+
+}
---
+++ app/src/main/java/com/lkintechnology/mBilling/adapters/PaymentShowListAdapter.java
@@ -0,0 +1,170 @@
+package com.lkintechnology.mBilling.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.lkintechnology.mBilling.R;
+
+import java.util.List;
+import java.util.Map;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+/**
+ * Created by manjooralam on 3/5/2018.
+ */
+
+public class PaymentShowListAdapter extends BaseAdapter {
+    Context context;
+    private List<Map> mListMap;
+
+    public PaymentShowListAdapter(Context context, List<Map> mListMap) {
+        this.context = context;
+        this.mListMap = mListMap;
+
+    }
+
+    @Override
+    public int getCount() {
+        return mListMap.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        PaymentShowListAdapter.ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_add_credit_note_item, parent, false);
+            holder = new PaymentShowListAdapter.ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (PaymentShowListAdapter.ViewHolder) convertView.getTag();
+        }
+        if ( mListMap.get(position).get("goodsItem").equals("")){
+            Map map=mListMap.get(position);
+            Timber.i("MEEEEEEEEEE"+mListMap.get(position));
+            String invoicedetail= (String) map.get("inv_num");
+            String date= (String) map.get("date");
+            String gst= (String) map.get("gst");
+            String cgst= (String) map.get("cgst");
+            String sgst= (String) map.get("sgst");
+            String differenciateitem= (String) map.get("difference_amount");
+            String igst= (String) map.get("igst");
+
+        /*String discount= (String) map.get("discount");
+        String value= (String) map.get("value");
+        String total= (String) map.get("total");
+        String mrp= (String) map.get("mrp");*/
+
+            holder.tvInvoiceDetail.setText(invoicedetail);
+            holder.tvDate.setText(date);
+            holder.tvGST.setText(gst);
+            holder.tvCCST.setText(cgst);
+            holder.tvDifferenciateItem.setText(differenciateitem);
+            holder.tvIGST.setText(igst);
+            // holder.tvITCEligibility(itcEligibility);
+            //   holder..setText(sgst);
+
+        }else {
+            holder.tvITCEligibility.setVisibility(View.VISIBLE);
+            holder.tvViewITCEligibility.setVisibility(View.VISIBLE);
+            Map map=mListMap.get(position);
+            Timber.i("MEEEEEEEEEE"+mListMap.get(position));
+            String invoicedetail= (String) map.get("inv_num");
+            String date= (String) map.get("date");
+            String gst= (String) map.get("gst");
+            String cgst= (String) map.get("cgst");
+            String sgst= (String) map.get("sgst");
+            String differenciateitem= (String) map.get("difference_amount");
+            String igst= (String) map.get("igst");
+            String itcEligibility= (String) map.get("goodsItem");
+        /*String discount= (String) map.get("discount");
+        String value= (String) map.get("value");
+        String total= (String) map.get("total");
+        String mrp= (String) map.get("mrp");*/
+
+            holder.tvInvoiceDetail.setText(invoicedetail);
+            holder.tvDate.setText(date);
+            holder.tvGST.setText(gst);
+            holder.tvCCST.setText(cgst);
+            holder.tvDifferenciateItem.setText(differenciateitem);
+            holder.tvIGST.setText(igst);
+            holder.tvSGST.setText(sgst);
+            holder.tvITCEligibility.setText(itcEligibility);
+            // holder.tvITCEligibility(itcEligibility);
+            //   holder..setText(sgst);
+        }
+        /*Map map=mListMap.get(position);
+        Timber.i("MEEEEEEEEEE"+mListMap.get(position));
+        String invoicedetail= (String) map.get("inv_num");
+        String date= (String) map.get("date");
+        String gst= (String) map.get("gst");
+        String cgst= (String) map.get("cgst");
+        String sgst= (String) map.get("sgst");
+        String differenciateitem= (String) map.get("difference_amount");
+        String igst= (String) map.get("igst");
+        String itcEligibility= (String) map.get("goodsItem");
+        *//*String discount= (String) map.get("discount");
+        String value= (String) map.get("value");
+        String total= (String) map.get("total");
+        String mrp= (String) map.get("mrp");*//*
+
+        holder.tvInvoiceDetail.setText(invoicedetail);
+        holder.tvDate.setText(date);
+        holder.tvGST.setText(gst);
+        holder.tvCCST.setText(cgst);
+        holder.tvDifferenciateItem.setText(differenciateitem);
+        holder.tvIGST.setText(igst);
+       // holder.tvITCEligibility(itcEligibility);
+     //   holder..setText(sgst);
+        return convertView;*/
+        return convertView;
+    }
+
+    class ViewHolder {
+        @Bind(R.id.tv_invoice_detail)
+        TextView tvInvoiceDetail;
+        @Bind(R.id.tv_date)
+        TextView tvDate;
+        @Bind(R.id.tv_differenciate_item)
+        TextView tvDifferenciateItem;
+        @Bind(R.id.tv_ccst)
+        TextView tvCCST;
+        @Bind(R.id.tv_gst)
+        TextView tvGST;
+        @Bind(R.id.tv_igst)
+        TextView tvIGST;
+        @Bind(R.id.tv_sgst)
+        TextView tvSGST;
+        @Bind(R.id.tv_itceligibility)
+        TextView tvITCEligibility;
+        @Bind(R.id.tv_view_igst)
+        TextView tvViewIGST;
+        @Bind(R.id.tv_view_itc_eligibility)
+        TextView tvViewITCEligibility;
+
+
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+
+        }
+    }
+}
+
