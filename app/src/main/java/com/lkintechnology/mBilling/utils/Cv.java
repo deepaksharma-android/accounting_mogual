4b9483500ddf3d8347392eb007af463c85391c8a
PUNAMCHAND <punam.chand@berylsystem.com>
2018-03-19 19:21:23 +0530

    EditText hint color change in vouchers

--- app/src/main/java/com/lkintechnology/mBilling/utils/Cv.java
+++ app/src/main/java/com/lkintechnology/mBilling/utils/Cv.java
@@ -4,9 +4,9 @@ package com.lkintechnology.mBilling.utils;
 public interface Cv {
 
 
-  // String BASE_URL = "https://mbilling.in/api/v1/";
+   //String BASE_URL = "https://mbilling.in/api/v1/";
   String BASE_URL = "http://accounts.geeksonrails.com/api/v1/";
-  //String BASE_URL = "http://192.168.1.8:3009/api/v1/";
+ // String BASE_URL = "http://192.168.1.13:3000/api/v1/";
 
 
 
