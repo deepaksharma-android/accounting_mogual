7f016399661fbaff8201714495e1c18223c923e5
PUNAMCHAND <punam.chand@berylsystem.com>
2018-06-06 14:27:41 +0530

    Gstr-1 and Gstr-2 api integrated with functionality

--- app/src/main/java/com/lkintechnology/mBilling/networks/Api.java
+++ app/src/main/java/com/lkintechnology/mBilling/networks/Api.java
@@ -682,6 +682,12 @@ public interface Api {
     @GET("company/gstr3b/{id}")
     Call<CompanyReportResponse> getgstr3b(@Path("id") String id, @Query("start_date") String start_date, @Query("end_date") String end_date);
 
+    @GET("company/gstr1/{id}")
+    Call<CompanyReportResponse> getgstr_1(@Path("id") String id, @Query("start_date") String start_date, @Query("end_date") String end_date);
+
+    @GET("company/gstr2/{id}")
+    Call<CompanyReportResponse> getgstr_2(@Path("id") String id, @Query("start_date") String start_date, @Query("end_date") String end_date);
+
     @GET("company_stock_transfer_vouchers/{id}")
     Call<GetStockTransferListResponse> getStockTransfer(@Path("id") String id, @Query("duration") String duration);
 
